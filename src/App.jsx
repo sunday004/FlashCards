d6f7399428f6a74ef6720da4e5d980f1285254db
Sunday Ochigbo <131619007+sunday004@users.noreply.github.com>
2023-10-07 02:00:19 -0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import './App.css';
+import GuessForm from './Components/GuessForm';
 
 const App = ()=>{
 
@@ -8,6 +9,7 @@ const App = ()=>{
   const [index, setIndex] = useState(10);
   const [classTitle, setClassName] = useState('flashCard')
   const [type, setType] = useState("")
+  
 
   const Questions = ["What is the normal rate of heart beat per day?","What is the approximate number of working parts in the eye?",
     "Mitosis is the process a single cell uses to divide into two new identical cells. What do we call each set of cells?","What kind of animal is an Orca?",
@@ -16,7 +18,7 @@ const App = ()=>{
     "Who was the first person to walk on the moon?","What phenomena keeps the planets in orbit around the sun?",
     'start'
   ]
-  const Answers = ['115,000','2 million','Parent cells and daughter cells','A whale','Newton',
+  const Answers = ['115000','2 million','Parent cells and daughter cells','Whale','Newton',
   'A lowercase rho (ρ)','Iron','Richter Scale','Neil Armstrong','Gravity','click next']
 
   const updateCount = ()=>{
@@ -24,18 +26,38 @@ const App = ()=>{
     setClassName(count % 2 == 1 ? 'flashCard' : 'flashCard flipped')
   };
   const updateCard = ()=>{
-    setIndex(Math.floor(Math.random() * 10));
-    setCount(0)
-    setClassName('flashCard')
-    if ('0123'.includes(index)){
-      setType('biology')
-    }else if ('45'.includes(index)){
-      setType('physics')
-    }else if('678910'.includes(index)){
-      setType('geography')
+    if (1 <= index){
+      setIndex(index - 1)
+      setCount(0)
+      setClassName('flashCard')
+      if ('0123'.includes(index-1)){
+        setType('biology')
+      }else if ('45'.includes(index-1)){
+        setType('physics')
+      }else if('6789'.includes(index-1)){
+        setType('geography')
+      }else{
+        setType('')
+      }
     }
     console.log(index)
-    console.log(count)
+  }
+  const reverseCard = () => {
+    if (index <= 9){
+      setIndex(index + 1)
+      setCount(0)
+      setClassName('flashCard')
+      if ('0123'.includes(index+1)){
+        setType('biology')
+      }else if ('45'.includes(index+1)){
+        setType('physics')
+      }else if('6789'.includes(index+1)){
+        setType('geography')
+      }else{
+        setType('')
+      }
+    }
+    console.log(index+1)
   }
   /* const updateType = ()=>{
     
@@ -46,6 +68,7 @@ const App = ()=>{
       <h2>Science and Geography</h2>
       <h4>How well vested are you in Science and Geography? Well let's find out</h4>
       <h5>Number of cards: 10</h5>
+    
 
       <div id={type} className={classTitle} onClick={updateCount}>
         <div className='front'> {index == 10 ? "" : "Question:" }<br /> {Questions[index]}</div>
@@ -53,6 +76,8 @@ const App = ()=>{
           <h3> {index == 10 ? "" : "Answer:" }<br /> {Answers[index]} </h3>
         </div>
       </div>
+      <GuessForm answer = {Answers[index]}/>
+      <button type='previous' className='previousCard' onClick={reverseCard}>back</button>
       <button type='next' onClick={updateCard} className='nextCard'>next</button>
     </div>
   )
